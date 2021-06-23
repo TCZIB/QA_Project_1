752280760ea8008f335e6dc58544718f1e142886
TCZIB <tcalvin00@outlook.com>
2021-06-23 14:14:34 +0000

    Database Expansion

--- main_app/sub_programs/models.py
+++ main_app/sub_programs/models.py
@@ -9,21 +9,12 @@ class Movies(db.Model):
     movie_runtime = db.Column(db.Integer(), nullable=False)
     movie_cover_art = db.Column(db.String(100))
 
-    reviews = db.relationship('Movie_Reviews', backref="movie")
+    reviews = db.relationship('MovieReviews', backref="movie")
 
-class Movie_Reviews(db.Model):
+class MovieReviews(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     
-    movie_id = db.Column(db.Integer, db.ForeignKey('movies.id'), nullable=False)
+    movie_id = db.Column(db.Integer, db.ForeignKey('Movies.id'), nullable=False)
     
     review_contents = db.Column(db.String(500), nullable=False)
     review_author = db.Column(db.String(30), nullable=False)
-
-    review_likes = db.relationship('Review_Likes', backref="movie_review")
-
-class Review_Likes(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    movie_review_id = db.Column(db.Integer, db.ForeignKey('movie_reviews.id'))
-    
-    like_status = db.Column(db.Boolean)
-    like_user = db.Column(db.String(15), nullable=False)
