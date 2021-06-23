d4d91f7fcd842e487a8eff99e5ffd0148123d0e6
TCZIB <tcalvin00@outlook.com>
2021-06-23 15:00:23 +0000

    Webpage Dev

--- main_app/sub_programs/models.py
+++ main_app/sub_programs/models.py
@@ -14,7 +14,16 @@ class Movies(db.Model):
 class MovieReviews(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     
-    movie_id = db.Column(db.Integer, db.ForeignKey('Movies.id'), nullable=False)
+    movie_id = db.Column(db.Integer, db.ForeignKey('movies.id'), nullable=False)
     
     review_contents = db.Column(db.String(500), nullable=False)
     review_author = db.Column(db.String(30), nullable=False)
+
+
+class ReviewLikes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+    review_id = db.Column(db.Integer, db.ForeignKey('movie_reviews.id'), nullable=False)
+
+    like_bool = db.Column(db.Boolean())
+    like_author = db.Column(db.String(30), nullable=False)
--- main_app/sub_programs/routes.py
+++ main_app/sub_programs/routes.py
@@ -1,8 +1,17 @@
 from sub_programs import app, db
 from flask import render_template, request, url_for
+from sub_programs.models import Movies
 
 @app.route("/")
 @app.route("/home")
 
 def home_page():
-    return "Welcome Home"
+    return render_template('index.html')
+
+@app.route("/movies")
+def view_movies():
+    return render_template('movies.html', items = Movies.query.all())
+
+@app.route("/movies/reviews/<movie_id>", methods=['GET', 'POST'])
+def movie_reviews(movie_id):
+    return movie_id
