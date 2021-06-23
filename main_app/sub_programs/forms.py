6dd9539f66eace50358d7005a27409477a5ee106
TCZIB <tcalvin00@outlook.com>
2021-06-23 16:27:55 +0000

    Reviews page being developed

---
+++ main_app/sub_programs/forms.py
@@ -0,0 +1,15 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, IntegerField, DateField, SubmitField
+from wtforms.validators import Length, DataRequired, ValidationError
+
+profanities = ["Shit", "Fuck"]
+
+class Movie_Review_Form(FlaskForm):
+    username = StringField("Your name: ", validators=[DataRequired(), Length(min=5, max=30)])
+    review = StringField("Your review: ", validators=[DataRequired(), Length(min=1, max=500)])
+    submit = SubmitField("Submit")
+
+    def validate_review_contents(self, review):
+        if profanities in review.lower():
+            raise ValidationError("Please make sure content is clean!")
+
--- main_app/sub_programs/routes.py
+++ main_app/sub_programs/routes.py
@@ -1,6 +1,7 @@
+from sub_programs import models
 from sub_programs import app, db
 from flask import render_template, request, url_for
-from sub_programs.models import Movies
+from sub_programs.models import Movies, MovieReviews, ReviewLikes 
 
 @app.route("/")
 @app.route("/home")
@@ -12,6 +13,25 @@ def home_page():
 def view_movies():
     return render_template('movies.html', items = Movies.query.all())
 
-@app.route("/movies/reviews/<movie_id>", methods=['GET', 'POST'])
-def movie_reviews(movie_id):
-    return movie_id
+@app.route("/movies/reviews/<movie_title>/<movie_id>", methods=['GET', 'POST'])
+def movie_reviews_page(movie_title, movie_id):
+    
+    selected_movie = Movies.query.get(int(movie_id))
+    number_of_reviews = 5
+
+    try:
+        selected_movie.movie_title
+    except:
+        return render_template("not_found.html")
+
+    try:
+        reviews = MovieReviews.query.filter_by(movie_id=int(selected_movie.id)).all()
+
+    except:
+        reviews = 0
+
+    return render_template("review_page.html", item=selected_movie, number_of_reviews=number_of_reviews, reviews=reviews)
+
+@app.route("/not_recognised")
+def not_recognised():
+    return render_template("not_found.html")
