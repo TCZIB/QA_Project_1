08fdf20f26fee7a730662e67e5ddc868cfaf48ec
TCZIB <tcalvin00@outlook.com>
2021-06-24 15:01:06 +0000

    Movies can now be added through the web

--- main_app/sub_programs/__init__.py
+++ main_app/sub_programs/__init__.py
@@ -10,4 +10,15 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+parentDirectory = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
+user_path = parentDirectory + "/main_app/sub_programs/static/blacklisted_words.txt"
+
+words = open(user_path, 'r')
+lines = words.readlines()
+
+profanities = []
+
+for line in lines:
+    profanities.append(line[:-1])
+
 from sub_programs import routes
--- main_app/sub_programs/routes.py
+++ main_app/sub_programs/routes.py
@@ -3,6 +3,7 @@ from sub_programs import models
 from sub_programs import app, db
 from flask import render_template, request, url_for, redirect
 from sub_programs.models import Movies, MovieReviews, ReviewLikes, Users
+from sub_programs import profanities
 
 @app.route("/")
 @app.route("/home")
@@ -20,6 +21,7 @@ def movie_reviews_page(movie_title, movie_id):
     form = Movie_Review_Form()
 
     selected_movie = Movies.query.get(movie_id)
+    
     try:
         selected_movie.movie_title
     except:
@@ -30,6 +32,26 @@ def movie_reviews_page(movie_title, movie_id):
 
     except:
         reviews = 0
+
+    if form.validate_on_submit():
+
+        review_content = form.review.data
+        review_author = form.username.data
+
+        review_content_test = review_content.split()
+
+        for word in review_content_test:
+            for bad_word in profanities:
+                if word == bad_word:
+                    message = f"{word} Not allowed!"
+                    return render_template("review_page.html", item=selected_movie, reviews=reviews, MovieReviews=MovieReviews, form=form, message=message)
+
+        new_review = MovieReviews(movie_id=selected_movie.id, review_contents=review_content, review_author=review_author)
+
+        db.session.add(new_review)
+        db.session.commit()
+
+        return render_template("review_page.html", item=selected_movie, reviews=reviews, MovieReviews=MovieReviews, form=form)
 
     return render_template("review_page.html", item=selected_movie, reviews=reviews, MovieReviews=MovieReviews, form=form)
 
@@ -68,7 +90,7 @@ def logged_in(user, password):
 
     if form.validate_on_submit():
     
-            form_movie_title = form.movie_cover_art.data
+            form_movie_title = form.movie_title.data
             form_movie_age = form.movie_age.data
             form_movie_description = form.movie_description.data
             form_movie_runtime = form.movie_runtime.data
@@ -76,23 +98,38 @@ def logged_in(user, password):
 
             if not form_movie_cover_art:
                 message = "Empty movie cover art"
-                return render_template("user_home.html",items = Movies.query.order_by(Movies.movie_title), user=user, password=password, message=message, form=form)
+                return render_template("user_home.html",items = Movies.query.order_by(Movies.movie_title), user=user, password=password, message_add_error=message, form=form)
 
             if not form_movie_title:
                 message = "Empty movie title"
-                return render_template("user_home.html",items = Movies.query.order_by(Movies.movie_title), user=user, password=password, message=message, form=form)
+                return render_template("user_home.html",items = Movies.query.order_by(Movies.movie_title), user=user, password=password, message_add_error=message, form=form)
 
             if not form_movie_age:
                 message = "Empty movie age - rating"
-                return render_template("user_home.html",items = Movies.query.order_by(Movies.movie_title), user=user, password=password, message=message, form=form)
+                return render_template("user_home.html",items = Movies.query.order_by(Movies.movie_title), user=user, password=password, message_add_error=message, form=form)
 
             if not form_movie_description:
                 message = "Empty movie description"
-                return render_template("user_home.html",items = Movies.query.order_by(Movies.movie_title), user=user, password=password, message=message, form=form)
+                return render_template("user_home.html",items = Movies.query.order_by(Movies.movie_title), user=user, password=password, message_add_error=message, form=form)
 
             if not form_movie_runtime:
                 message = "Empty movie runtime"
-                return redirect("user_home.html",items = Movies.query.order_by(Movies.movie_title), user=user, password=password, message=message, form=form)
+                return render_template("user_home.html",items = Movies.query.order_by(Movies.movie_title), user=user, password=password, message=message, form=form)
+            
+            try:
+                int(form.movie_runtime.data)
+            except:
+                message = "Runtime must be interger!"
+                return render_template("user_home.html",items = Movies.query.order_by(Movies.movie_title), user=user, password=password, message=message, form=form)
+
+            new_item = Movies(movie_title=form_movie_title, movie_age=form_movie_age, movie_description=form_movie_description, movie_runtime= form_movie_runtime, movie_cover_art=form_movie_cover_art)
+
+            db.session.add(new_item)
+            db.session.commit()
+
+            message = "Item added"
+            return render_template("user_home.html",items = Movies.query.order_by(Movies.movie_title), user=user, password=password, message=message, form=form)
+
 
     return render_template("user_home.html",items = Movies.query.order_by(Movies.movie_title), user=user, password=password, message="", form=form)
    
@@ -111,7 +148,7 @@ def modify_movie(movie_id, user):
         
         if form.cancel.data:
 
-            return render_template("user_home.html",items = Movies.query.order_by(Movies.movie_title), user=user, password=password, message="")
+            return redirect(url_for("logged_in", user=user, password=password))
 
         elif form.modify.data:
 
@@ -119,7 +156,7 @@ def modify_movie(movie_id, user):
             # This means that is you wanted to cancel the modification it would try to vaildate but couldnt so would fail
             # Instead it validates on the modift/update button press
 
-            form_movie_title = form.movie_cover_art.data
+            form_movie_title = form.title.data
             form_movie_age = form.movie_age.data
             form_movie_description = form.movie_description.data
             form_movie_runtime = form.movie_runtime.data
@@ -144,6 +181,12 @@ def modify_movie(movie_id, user):
             if not form_movie_runtime:
                 message = "Empty movie runtime"
                 return render_template("modify.html", form=form, item=item_to_modify, user=user, password=password, message=message)
+            
+            try:
+                int(form.movie_runtime.data)
+            except:
+                message = "Runtime must be interger!"
+                return redirect("user_home.html",items = Movies.query.order_by(Movies.movie_title), user=user, password=password, message=message, form=form)
 
             item_to_modify.movie_cover_art = form.movie_cover_art.data
             item_to_modify.movie_title = form.movie_title.data
@@ -167,7 +210,11 @@ def modify_movie(movie_id, user):
             db.session.delete(item_to_modify)
             db.session.commit()
             
-            return render_template("user_home.html",items = Movies.query.order_by(Movies.movie_title), user=user, password=password, message=f"{item_to_modify.movie_title} Deleted")
+            message = f"{item_to_modify.movie_title} Deleted"
+
+            return redirect(url_for("logged_in", user=user, password=password, message=message))
+
+            #return render_template("user_home.html",items = Movies.query.order_by(Movies.movie_title), user=user, password=password, message=f"{item_to_modify.movie_title} Deleted")
 
     else:
 
