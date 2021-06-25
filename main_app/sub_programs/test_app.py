cb31cb21c5d362a317aabc609c87bd0c350e22b5
TCZIB <tcalvin00@outlook.com>
2021-06-25 10:49:46 +0000

    Basic testing initiated

--- main_app/sub_programs/test_app.py
+++ main_app/sub_programs/test_app.py
@@ -1,10 +1,20 @@
+from flask import url_for
 from flask_testing import TestCase
+import os
+
+# import the app's classes and objects
+from sub_programs import app
 
 class TestBase(TestCase):
 
     def create_app(self):
-    # Pass in configurations for tests
-        pass
+        # Pass in testing configurations for the app. Here we use sqlite without a persistent database for our tests.
+        app.config.update(SQLALCHEMY_DATABASE_URI=os.getenv("databaseLogin"),
+                SECRET_KEY='TEST_SECRET_KEY',
+                DEBUG=True,
+                WTF_CSRF_ENABLED=False
+                )
+        return app
 
     def setUp(self):
     #  Will be called before every test
